c0129bfa392f6e33b82d5b111a6f7302857d1639
medbaz <mohamed.bazber11@gmail.com>
2024-08-28 17:07:27 +0200

    .

--- backendBooking/src/index.ts
+++ backendBooking/src/index.ts
@@ -49,7 +49,7 @@ app.use(cors({
 app.use(express.static(path.join(__dirname, '../../frontendbooking/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontendBooking/dist', 'index.html'));
+  res.sendFile(path.join(__dirname,  'index.html'));
 });
 app.use('/api/users',userRouter)
 app.use('/api/auth',authRouter)
