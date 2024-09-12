34e967db9a105909a2da29a518b4017d53a4e38e
medbaz <mohamed.bazber11@gmail.com>
2024-09-13 00:17:21 +0200

    e

--- backendBooking/dist/models/hotel.js
+++ backendBooking/dist/models/hotel.js
@@ -1,65 +1,61 @@
 "use strict";
-// import mongoose from "mongoose";
 Object.defineProperty(exports, "__esModule", { value: true });
-// const hotelSchema = new mongoose.Schema({
-//     userId:{
-//         type:String,
-//         required:true
-//     },
-//     name:{
-//         type:String,
-//         required:true
-//     },
-//     city:{
-//         type:String,
-//         required:true
-//     },
-//     country:{
-//         type:String,
-//         required:true
-//     },
-//     description:{
-//         type:String,
-//         required:true
-//     },
-//     type:{
-//         type:String,
-//         required:true
-//     },
-//     adultCount:{
-//         type:Number,
-//         required:true
-//     }
-//     ,
-//     childCount:{
-//         type:Number,
-//         required:true
-//     }
-//     ,
-//     facilities:[{
-//         type:String,
-//         required:true
-//     }]
-//     ,
-//     pricePerNight:{
-//         type:Number,
-//         required:true
-//     } ,
-//     starRating:{
-//         type:Number,
-//         required:true ,
-//         min:1 ,
-//         max:5
-//     } ,
-//     imageUrls:[{
-//         type:String,
-//         required:true
-//     }],
-//     lastUpdated:{
-//         type:Date,
-//         required:true
-//     }
-// }
-// )
-// const Hotel = mongoose.model('Hotel',hotelSchema)
-// export default Hotel
+const mongoose = require("mongoose");
+const hotelSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    adultCount: {
+        type: Number,
+        required: true
+    },
+    childCount: {
+        type: Number,
+        required: true
+    },
+    facilities: [{
+            type: String,
+            required: true
+        }],
+    pricePerNight: {
+        type: Number,
+        required: true
+    },
+    starRating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    imageUrls: [{
+            type: String,
+            required: true
+        }],
+    lastUpdated: {
+        type: Date,
+        required: true
+    }
+});
+const Hotel = mongoose.model('Hotel', hotelSchema);
+exports.default = Hotel;
